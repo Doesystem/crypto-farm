879ce5354b43be38e14fb6c66fb147f2c21b21de
Doesystem <doe.system@gmail.com>
2023-06-08 02:31:30 +0700

    show config and call packageversion

--- pages/test-registry.tsx
+++ pages/test-registry.tsx
@@ -1,28 +1,43 @@
 import Layout from "@/app/Layout/layout";
 import {useEffect, useState} from "react";
-import {getRegistry} from "@typus/typus-sdk/lib/utils/typus-dov-single/registry";
+import {getPackageVersion, getRegistry} from "@typus/typus-sdk/lib/utils/typus-dov-single/registry";
 import {Connection, JsonRpcProvider} from "@mysten/sui.js";
 
 const provider = new JsonRpcProvider(new Connection({fullnode: process.env.RPC_ENDPOINT}));
 
 export default function TestRegistry() {
-    const [data, setData] = useState()
+    const [registry, setRegistry] = useState()
+    const [packageVersion, setPackageVersion] = useState()
 
     useEffect(() => {
         (async () => {
-            let result = await getRegistry(provider, process.env.SINGLE_COLLATERAL_REGISTRY);
-            let data = JSON.stringify(result, (_, v) => (typeof v === "bigint" ? `${v}` : v), 2)
-            setData(data)
+            let resultRegistry = await getRegistry(provider, process.env.SINGLE_COLLATERAL_REGISTRY);
+            let dataRegistry = JSON.stringify(resultRegistry, (_, v) => (typeof v === "bigint" ? `${v}` : v), 2)
+
+            let resultPckageVersion = await getPackageVersion(provider, process.env.SINGLE_COLLATERAL_PACKAGE);
+            let dataPackageVersion = JSON.stringify(resultPckageVersion, (_, v) => (typeof v === "bigint" ? `${v}` : v), 2)
+
+            setRegistry(dataRegistry)
+            setPackageVersion(dataPackageVersion)
         })();
     }, [])
 
     return (
         <Layout>
-            <br/>
-            <div>getRegistry</div>
-            <small>
-                <pre>{data}</pre>
-            </small>
+            <div className={'rounded-lg shadow-md m-3 p-3'}>
+                <h3>getRegistry</h3>
+                <small>
+                    <pre>{registry}</pre>
+                </small>
+            </div>
+
+            <div className={'rounded-lg shadow-md m-3 p-3'}>
+                <h3>getPackageVersion</h3>
+                <small>
+                    <pre>{packageVersion}</pre>
+                </small>
+            </div>
+
         </Layout>
     )
 }
--- src/app/Layout/navbar.tsx
+++ src/app/Layout/navbar.tsx
@@ -1,12 +1,48 @@
 import Link from "next/link";
 
 export default function Navbar() {
+    let version = process.env.VERSION
+    let dbName = process.env.DB_NAME
+    let rpcEndpoint = process.env.RPC_ENDPOINT
+    let singleCollateralPackage = process.env.SINGLE_COLLATERAL_PACKAGE
+    let singleCollateralPackageOrigin = process.env.SINGLE_COLLATERAL_PACKAGE_ORIGIN
+    let singleCollateralManagerCap = process.env.SINGLE_COLLATERAL_MANAGER_CAP
+    let singleCollateralRegistry = process.env.SINGLE_COLLATERAL_REGISTRY
+    let singleCollateralDepositVaultRegistry = process.env.SINGLE_COLLATERAL_DEPOSIT_VAULT_REGISTRY
+    let singleCollateralBidVaultRegistry = process.env.SINGLE_COLLATERAL_BID_VAULT_REGISTRY
+    let oraclePackage = process.env.ORACLE_PACKAGE
+    let oracleManagerCap = process.env.ORACLE_MANAGER_CAP
+    let suiOracle = process.env.SUI_ORACLE
+    let cetusOracle = process.env.CETUS_ORACLE
+
     return (
-        <>
+        <div className={'rounded-lg shadow-md bg-slate-100'}>
             <div>
-                <Link className={'border-2 px-2'} href={'/'}>Home</Link>
-                <Link className={'border-2 px-2'} href={'/test-registry'}>Test registry</Link>
+                <Link className={'p-2 rounded-lg shadow-md bg-slate-200'} href={'/'}>Home</Link>
+                <Link className={'p-2 rounded-lg shadow-md bg-slate-200'} href={'/test-registry'}>Test registry</Link>
             </div>
-        </>
+            <ul className={'p-3 text-xs'}>
+                <li>VERSION: <span className={'font-bold bg-slate-200'}>{version}</span></li>
+                <li>DB_NAME: <span className={'font-bold bg-slate-200'}>{dbName}</span></li>
+                <li>RPC_ENDPOINT: <span className={'font-bold bg-slate-200'}>{rpcEndpoint}</span></li>
+                <li>SINGLE_COLLATERAL_PACKAGE: <span
+                    className={'font-bold bg-slate-200'}>{singleCollateralPackage}</span></li>
+                <li>SINGLE_COLLATERAL_PACKAGE_ORIGIN: <span
+                    className={'font-bold bg-slate-200'}>{singleCollateralPackageOrigin}</span></li>
+                <li>SINGLE_COLLATERAL_MANAGER_CAP: <span
+                    className={'font-bold bg-slate-200'}>{singleCollateralManagerCap}</span>
+                </li>
+                <li>SINGLE_COLLATERAL_REGISTRY: <span
+                    className={'font-bold bg-slate-200'}>{singleCollateralRegistry}</span></li>
+                <li>SINGLE_COLLATERAL_DEPOSIT_VAULT_REGISTRY: <span
+                    className={'font-bold bg-slate-200'}>{singleCollateralDepositVaultRegistry}</span></li>
+                <li>SINGLE_COLLATERAL_BID_VAULT_REGISTRY: <span
+                    className={'font-bold bg-slate-200'}>{singleCollateralBidVaultRegistry}</span></li>
+                <li>ORACLE_PACKAGE: <span className={'font-bold bg-slate-200'}>{oraclePackage}</span></li>
+                <li>ORACLE_MANAGER_CAP: <span className={'font-bold bg-slate-200'}>{oracleManagerCap}</span></li>
+                <li>SUI_ORACLE: <span className={'font-bold bg-slate-200'}>{suiOracle}</span></li>
+                <li>CETUS_ORACLE : <span className={'font-bold bg-slate-200'}>{cetusOracle}</span></li>
+            </ul>
+        </div>
     )
 }
